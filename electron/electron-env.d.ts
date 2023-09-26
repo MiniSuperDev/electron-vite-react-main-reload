2793a6831a87a5da20a279a0b549aa3c53858249
MiniSuperDev <105388540+MiniSuperDev@users.noreply.github.com>
2023-09-26 12:51:23 -0500

    feat: add ipc demo

--- electron/electron-env.d.ts
+++ electron/electron-env.d.ts
@@ -2,10 +2,16 @@
 
 declare namespace NodeJS {
   interface ProcessEnv {
-    VSCODE_DEBUG?: 'true'
-    DIST_ELECTRON: string
-    DIST: string
+    VSCODE_DEBUG?: "true";
+    DIST_ELECTRON: string;
+    DIST: string;
     /** /dist/ or /public/ */
-    VITE_PUBLIC: string
+    VITE_PUBLIC: string;
   }
+}
+
+interface Window {
+  api: {
+    getDate: () => Promise<string>;
+  };
 }
